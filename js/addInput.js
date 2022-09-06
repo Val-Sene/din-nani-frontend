a6da854f45cec37ae4100f8cebc9997333a3066c
val-sene <adjeivalentine11@gmail.com>
2022-09-06 13:04:46 +0000

    all pages linked together and media size updated

--- js/addInput.js
+++ js/addInput.js
@@ -8,15 +8,15 @@
       document.getElementById("main").append(input);
   }
 
-  var a = 1;
-  button_add_row1.onclick = function(e){
-      e.preventDefault();
-      a++;
-           var input = document.createElement("div");
-      var dtd = '<div></div>';
-      input.innerHTML = document.getElementById('subinput1').innerHTML + dtd;
-      document.getElementById("main1").append(input);
-  }
+  // var a = 1;
+  // button_add_row1.onclick = function(e){
+  //     e.preventDefault();
+  //     a++;
+  //          var input = document.createElement("div");
+  //     var dtd = '<div></div>';
+  //     input.innerHTML = document.getElementById('subinput1').innerHTML + dtd;
+  //     document.getElementById("main1").append(input);
+  // }
 
 //   button_remove_row.onclick =  function(e){
 //      e.preventDefault();
